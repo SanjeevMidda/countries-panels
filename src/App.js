0aa5dc5514abaf374d961cc99d85eb58b8c511e7
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 20:38:58 +0000

    refactored .mainPanelContainer and mapped panels inside

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Panel from "./components/Panel";
+import TEXTDATA from "./data/TextData";
+import COLORS from "./data/Colors";
 
 const noOfPanels = Array.from({ length: 5 }, (_, index) => index);
 
@@ -13,6 +15,10 @@ function App() {
   return (
     <div className="App">
       <div className="mainPanelContainer">
+        {noOfPanels.map((p) => {
+          return <Panel id={p.id} title={p.title} area={p.area} />;
+        })}
+
         <Panel />
         <Panel />
         <Panel />
