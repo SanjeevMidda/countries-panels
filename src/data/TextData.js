13ec5489e2add080f0a74855507a9dbf1ac6f64d
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 20:35:55 +0000

    added id to each panel to prevent map errors

--- src/data/TextData.js
+++ src/data/TextData.js
@@ -1,22 +1,29 @@
 const TEXTDATA = [
   {
+    id: 1,
     title: "SEOUL",
     area: "A / 1",
   },
   {
+    id: 2,
     title: "LONDON",
     area: "A / 2",
   },
   {
+    id: 3,
     title: "PARIS",
     area: "A / 3",
   },
   {
+    id: 4,
     title: "RIO",
     area: "A / 4",
   },
   {
+    id: 5,
     title: "TOKYO",
     area: "A / 5",
   },
 ];
+
+export default TEXTDATA;
