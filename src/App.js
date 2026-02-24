35d7821fcd677b4d24bdca8ff811bfd2e4043c4b
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 20:23:28 +0000

    created styles for the panel and circle elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
+import Panel from "./components/Panel";
 
 function App() {
   // create panel component
   // provide props to these
   // text, area, circle
-  // create main UI
+  // create movement
 
   return (
     <div className="App">
-      <div className="mainPanelContainer"></div>
+      <div className="mainPanelContainer">
+        <Panel />
+        <Panel />
+        <Panel />
+        <Panel />
+        <Panel />
+      </div>
     </div>
   );
 }
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Panel = ({ title, area, colors }) => {
-  return <div className="panel">Panel</div>;
+  return (
+    <div className="panel">
+      <div className="textContent"></div>
+      <div className="circle"></div>
+    </div>
+  );
 };
 
 export default Panel;
