46db7584ba5c2be725dd78ed9d1bd1f29bd41caa
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 21:01:37 +0000

    created state to determine position of panel

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Panel = ({ title, area, colors }) => {
+  const [position, setPosition] = useState(false);
+
   return (
-    <div className="panel">
-      <div className="textContent"></div>
-      <div className="circle"></div>
+    <div
+      className="panel"
+      style={{ transform: `translateY(${position ? "-100px" : "0px"})` }}
+    >
+      <div className="textContent">
+        <h3>{title}</h3>
+        <h5>{area}</h5>
+      </div>
+      <div
+        className="circle"
+        style={{
+          background: `linear-gradient(${colors[0]}, ${colors[1]})`,
+        }}
+      ></div>
     </div>
   );
 };
