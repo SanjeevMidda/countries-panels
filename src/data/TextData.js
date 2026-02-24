579a4c7b9f9935058ec9b11e39286940166d899c
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 20:27:05 +0000

    created separate file to hold our panel data

---
+++ src/data/TextData.js
@@ -0,0 +1,22 @@
+const TEXTDATA = [
+  {
+    title: "SEOUL",
+    area: "A / 1",
+  },
+  {
+    title: "LONDON",
+    area: "A / 2",
+  },
+  {
+    title: "PARIS",
+    area: "A / 3",
+  },
+  {
+    title: "RIO",
+    area: "A / 4",
+  },
+  {
+    title: "TOKYO",
+    area: "A / 5",
+  },
+];
