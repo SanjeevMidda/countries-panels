ef168a899c4c5f467a6319df222022ddfafe3b38
Sanjeev Midda <s.midda1@gmail.com>
2026-02-24 21:24:05 +0000

    refactored ranbdomColor and incorporated useMemo to prevent re-rendering of values

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import "./App.css";
 import Panel from "./components/Panel";
 import TEXTDATA from "./data/TextData";
 import COLORS from "./data/Colors";
+import { useMemo } from "react";
 
 function App() {
-  // create panel component
-  // provide props to these
-  // text, area, circle
-  // create movement
-
-  const randomColor = () => {
-    const colorOne = COLORS[Math.floor(Math.random() * COLORS.length)];
-    const colorTwo = COLORS[Math.floor(Math.random() * COLORS.length)];
-
-    return [colorOne, colorTwo];
-  };
+  const panels = useMemo(() => {
+    return TEXTDATA.map((p) => ({
+      ...p,
+      colors: [
+        COLORS[Math.floor(Math.random() * COLORS.length)],
+        COLORS[Math.floor(Math.random() * COLORS.length)],
+      ],
+    }));
+  }, []);
 
   return (
     <div className="App">
       <div className="mainPanelContainer">
-        {TEXTDATA.map((p) => {
-          return <Panel key={p.id} title={p.title} area={p.area} />;
+        {panels.map((p) => {
+          return (
+            <Panel key={p.id} title={p.title} area={p.area} colors={p.colors} />
+          );
         })}
       </div>
     </div>
